89680423677a28992117d5e1fc42cc5703793d36
MarkoJ <jokovicmarko996@gmail.com>
2024-07-09 17:56:36 +0200

    completed nextjs course by verce

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+// export default function Page() {
+//   return <p>Customers Page</p>;
+// }
+
+import CardWrapper from "@/app/ui/dashboard/cards";
+// ...
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from "@/app/ui/skeletons";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      // ...
+    </main>
+  );
 }
